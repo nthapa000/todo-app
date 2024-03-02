8142b05aad5edce30b415253fa0488b2b38a0cb8
nthapa000 <nishantthapa0000@gmail.com>
2024-03-02 17:00:17 +0530

    Backend Done

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@
 // write express.json() middleware
 const express = require("express")
 const {createTodo, updateTodo} = require("./types")
+const {todo} = require("./db")
 // destructuring of object
 
 // we can also do it like this 
@@ -14,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 
-app.post("/todo", function (req,res){
+app.post("/todo", async function (req,res){
     // validation
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
@@ -25,11 +26,35 @@ app.post("/todo", function (req,res){
         return;
     }
     // put it in mongodb
+    // await syntax only inside async function 
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed:false
+    })
+    // if we don't await , then it like saying todo is inserted without todo waiting for database.
+    // await for todo (to actually reach database) , then only send the message
+    // if it throws exception in case something happens to database. 
+    res.json({
+        msg:"Todo created"
+    })
 })
-app.get("/todos",function(req,res){
 
+app.get("/todos",async function(req,res){
+// getting the todo
+const todos =await todo.find({});
+// const todos = todo.find({
+// title:"go to gym Boi!!"
+// });
+// If we want to do specific search
+// console.log(todos). this will log as promise 
+// because todo.find() need to actually hit the database
+// it will take long time we have to await for it. If we log right now what we get is promise which eventually resolve with the data.
+res.json({
+    todos
 })
-app.post("/completed",function(req,res){
+})
+app.post("/completed",async function(req,res){
     // marking todo as complicated
     const updatePayload = req.body;
     const parsedPayload = updateTodo.safeParse(updatePayload);
@@ -39,4 +64,14 @@ app.post("/completed",function(req,res){
         })
         return;
     }
+    await todo.update({
+        // automatically generated
+        _id: req.body.id
+    },{
+        // marked the _id todo as completed
+        completed:true
+    })
+    res.json({
+        msg:"Todo marked as completed"
+    })
 })
