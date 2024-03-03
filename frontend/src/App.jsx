dc6cd68b6e0d462941ace37d841aed34ba9c3d88
nthapa000 <nishantthapa0000@gmail.com>
2024-03-03 14:30:00 +0530

    DisplayingFromDatabase and Cors error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
+// import { useState } from "react"
 import { CreateTodo } from "./components/CreateTodo"
 import { Todos } from "./components/Todos"
 
+
 function App() {
+  // const [todos,setTodos] = useState([])
+
+  // fetch("http://localhost:3000/todos")
+  // .then(async function(res){
+  //   const json = await res.json();
+  //   setTodos(json.todos);
+  // })
+
   return (
     <div>
       <CreateTodo></CreateTodo>
-      <Todos todos={[
-        {
-          title:"Go to room",
-          description:"In 15 minutes",
-          completed:false
-        }
-      ]}></Todos>
+      <Todos todos={[]}></Todos>
     </div>
   )
 }
